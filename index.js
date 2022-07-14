34c6d3ac556f5561725fd7064d165962f78db3f5
Liza_silent <elizavetad@ya.ru>
2022-07-14 17:46:31 +0300

    more api

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-
-
-fetch('http://www.boredapi.com/api/activity/', {
-  method: 'GET'
-  })
-.then((res) => {
-  return res.json(); // возвращаем результат работы метода и идём в следующий then
-})
-.then((data) => {
-  console.log(data);
-  console.log(data.activity);// если мы попали в этот then, data — это объект
-})
-.catch((err) => {
-  console.log('Ошибка. Запрос не выполнен');
-});
-
-
 const cardQuote = document.querySelector(".card__quote");
+const cardBtn = document.querySelector(".card__button");
 
+
+
+function getActivity() {
+  fetch('http://www.boredapi.com/api/activity/', {
+    method: 'GET'
+    })
+  .then((res) => {
+    return res.json(); // возвращаем результат работы метода и идём в следующий then
+  })
+  .then((data) => { // если мы попали в этот then, data — это объект
+    console.log(data);
+    console.log(data.activity);
+
+    cardQuote.textContent = data.activity;
+  })
+  .catch((err) => {
+    console.log('Ошибка. Запрос не выполнен');
+  });
+}
+
+cardBtn.addEventListener("click", getActivity);
