24390ca0c9cb3a38029e1581f9ba2073b1d17a44
lizasilent <elizavetad@yandex.ru>
2022-07-17 17:33:35 +0300

    some shapes and designs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const cardQuote = document.querySelector(".card__quote");
 const cardBtn = document.querySelector(".card__button");
 
@@ -26,18 +28,24 @@ function getActivity() {
 cardBtn.addEventListener("click", getActivity);
 
 
-function getWeather() {
-  fetch('', {
-    method: 'GET'
-    })
-  .then((res) => {
-    return res.json(); // возвращаем результат работы метода и идём в следующий then
-  })
-  .then((data) => { // если мы попали в этот then, data — это объект
-    console.log(data);
+// function getWeather() {
+//   fetch('https://api.weather.yandex.ru/v2/informers?lat=59.9386&lon=30.3141&lang=en_US',
+//   {
+//     method: 'GET',
+//     headers: {
+//       'X-Yandex-API-Key': "7928440d-89d9-41c2-9640-499a09c7572e",
+//     },
+//   })
+//   .then((res) => {
+//     return res.json(); // возвращаем результат работы метода и идём в следующий then
+//   })
+//   .then((data) => { // если мы попали в этот then, data — это объект
+//     console.log(data);
 
-  })
-  .catch((err) => {
-    console.log('Ошибка. Запрос не выполнен' + err);
-  });
-}
+//   })
+//   .catch((err) => {
+//     console.log('Ошибка. Запрос не выполнен' + err);
+//   });
+// }
+
+// getWeather();
