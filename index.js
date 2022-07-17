e2d5f2747adeadfcefdd518e7aca9c3fed1c4249
lizasilent <elizavetad@yandex.ru>
2022-07-17 15:46:45 +0300

    api

--- index.js
+++ index.js
@@ -26,40 +26,18 @@ function getActivity() {
 cardBtn.addEventListener("click", getActivity);
 
 
-
-// создаёт разметку для поста
-function createPostMarkup(post) {
-  return `
-    <div class="post">
-      <p class="post__title">${post.title}</p>
-      <p class="post__text">${post.body}</p>
-    </div>
-  `;
-}
-
-// вставляет разметку в DOM
-function addPostToDOM(container, markup) {
-  container.insertAdjacentHTML('afterend', markup);
-}
-
-// Получает данные с сервера
-function getPosts() {
-fetch('https://jsonplaceholder.typicode.com/posts')
+function getWeather() {
+  fetch('', {
+    method: 'GET'
+    })
   .then((res) => {
-    return res.json();
+    return res.json(); // возвращаем результат работы метода и идём в следующий then
   })
-  .then((posts) =>
-  {
-    posts.forEach((post) => { addPostToDOM(document.querySelector(".template"), createPostMarkup(post))})
-  }
+  .then((data) => { // если мы попали в этот then, data — это объект
+    console.log(data);
 
-    )
-
-
+  })
   .catch((err) => {
-    console.log('Ошибка. Запрос не выполнен: ', err);
+    console.log('Ошибка. Запрос не выполнен' + err);
   });
 }
-
-getPosts();
-
