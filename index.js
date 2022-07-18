0f227d0ac5258de6c7841c923555eaac2575fe1b
Liza_silent <elizavetad@ya.ru>
2022-07-18 15:45:14 +0300

    card form

--- index.js
+++ index.js
@@ -1,53 +1,42 @@
-
-
 const cardQuote = document.querySelector(".card__quote");
 const cardBtn = document.querySelector(".card__button");
 const loader = document.querySelector(".loader");
 
-
 function getActivity() {
-  fetch('http://www.boredapi.com/api/activity/', {
-    method: 'GET'
+  fetch("http://www.boredapi.com/api/activity/", {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json(); // возвращаем результат работы метода и идём в следующий then
     })
-  .then((res) => {
-    return res.json(); // возвращаем результат работы метода и идём в следующий then
-  })
-  .then((data) => { // если мы попали в этот then, data — это объект
-    showSpinner(true);
-    cardRender(data);
-   
-  })
-  .catch((err) => {
-    console.log('Ошибка. Запрос не выполнен' + err);
-  })
-  .finally(() => {
-    showSpinner(false);
-  })
-  ;
+    .then((data) => {
+      // если мы попали в этот then, data — это объект
+      showSpinner(true);
+      cardRender(data);
+    })
+    .catch((err) => {
+      console.log("Ошибка. Запрос не выполнен" + err);
+      console.log(err);
+    })
+    .finally(() => {
+      showSpinner(false);
+    });
 }
 
 cardBtn.addEventListener("click", getActivity);
 
-
 function cardRender(data) {
   cardQuote.textContent = data.activity;
   cardBtn.textContent = "What else?";
-  console.log(data);
-  console.log(data.activity);
 }
-
 
 function showSpinner(isLoading) {
-if (isLoading) {
-loader.classList.remove("loader_hidden");
+  if (isLoading) {
+    loader.classList.remove("loader_hidden");
+  } else {
+    loader.classList.add("loader_hidden");
+  }
 }
-else {
-loader.classList.add("loader_hidden");
-}
-
-}
-
-
 
 // function getWeather() {
 //   fetch('https://api.weather.yandex.ru/v2/informers?lat=59.9386&lon=30.3141&lang=en_US',
